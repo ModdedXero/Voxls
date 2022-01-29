1af90a6efad0d8ef47f65a31df88f0938902d235
ModdedXero <53191205+ModdedXero@users.noreply.github.com>
2022-01-28 18:00:36 -0700

    Added premake

--- Voxls/src/Window.cpp
+++ Voxls/src/Window.cpp
@@ -38,20 +38,6 @@ namespace Voxls
 		glfwTerminate();
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 6510e0961814f9b1a5382281f59f96561e9d21b1
-	bool Window::IsRunning()
-	{
-
-	}
-
-<<<<<<< HEAD
->>>>>>> 6510e0961814f9b1a5382281f59f96561e9d21b1
-=======
->>>>>>> 6510e0961814f9b1a5382281f59f96561e9d21b1
 	Window* Window::Create(const WindowProps& props)
 	{
 		return new Window(props);
